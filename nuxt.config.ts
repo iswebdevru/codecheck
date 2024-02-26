539058e69f2812580260143009e4d7301cbb4de0
GreenBabyBorn <greenbabyborn@gmail.com>
2024-02-26 12:33:10 +0300

    add: mofavicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,26 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "ru",
       },
-      link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          sizes: "32x32",
+          type: "image/png",
+          href: "favicon-32x32.png",
+        },
+        {
+          rel: "icon",
+          sizes: "16x16",
+          type: "image/png",
+          href: "/favicon-16x16.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest" },
+      ],
     },
   },
 
